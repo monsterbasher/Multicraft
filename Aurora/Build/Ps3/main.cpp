1b91ef5750ad4a27538d4763b3e41587b4d9b6ec
Marcin Ploska <marcin.ploska@gmail.com>
2012-02-18 13:01:48 +0100

    Added draft for PS3 build.

---
+++ Aurora/Build/Ps3/main.cpp
@@ -0,0 +1,20 @@
+#include <cell/fs/cell_fs_file_api.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <sys/sys_time.h>
+#include <sys/process.h>
+#include <sys/spu_initialize.h>
+#include <sys/paths.h>
+#include <PSGL/psgl.h>
+#include <PSGL/psglu.h>
+#include <Cg/cg.h>
+#include <cell/dbgfont.h>
+
+SYS_PROCESS_PARAM(1001, 0x10000)
+
+int main()
+{
+	return 0;
+}
+
